f067e1340ba544863dd7ec2d0564bcc53ff758f0
Shah Arbaaz Jaan <arbaazaj1@gmail.com>
2023-02-24 11:52:31 +0530

    > Added Colors. > Removed boilerplate code. > Structured project directory. > Set the app default theme to dark mode, As the app has high contrast theme.

---
+++ lib/themes/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const Color darkOrange = Color(0xFFE38035);
+const Color grey = Color(0xFF363841);
+const Color darkGrey = Color(0xFF24252b);
+const Color veryDarkGrey = Color(0xFF131519);
+const Color black = Color(0xFF000000);
+const Color white = Color(0xFFFFFFFF);
