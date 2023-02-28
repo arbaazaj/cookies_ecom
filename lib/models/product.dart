755416be1c6f7455cdf2f024a9eca16be6398da0
Shah Arbaaz Jaan <arbaazaj1@gmail.com>
2023-03-01 02:17:52 +0530

    > Added Product Page. > Added a model class for product.

---
+++ lib/models/product.dart
@@ -0,0 +1,8 @@
+class Product {
+  final String name;
+  final String productImage;
+  final int price;
+  final bool isPremium;
+
+  Product(this.name, this.productImage, this.price, this.isPremium);
+}
