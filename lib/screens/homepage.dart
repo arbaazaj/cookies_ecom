43852e8630a7f06537a99699a898995c34d3dfc7
Shah Arbaaz Jaan <arbaazaj1@gmail.com>
2023-03-01 12:22:08 +0530

    > Working on cookie card.

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -18,6 +18,7 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       backgroundColor: backgroundColor,
       body: Column(
+        mainAxisSize: MainAxisSize.max,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           const CustomAppBar(),
@@ -27,49 +28,97 @@ class _HomePageState extends State<HomePage> {
           ),
           const SizedBox(height: 20.0),
           Padding(
-            padding: const EdgeInsets.only(left: 32.0, right: 32.0),
+            // TODO: Need to uncomment right padding later.
+            padding: const EdgeInsets.only(left: 32.0, /*right: 32.0*/),
             child: Stack(
-              clipBehavior: Clip.none,
+              alignment: Alignment.center,
               fit: StackFit.loose,
+              clipBehavior: Clip.none,
               children: [
-                Positioned.fill(
-                  top: 120,
-                  bottom: -120,
-                  child: Align(
-                    alignment: Alignment.centerLeft,
-                    child: Container(
-                      height: 330,
-                      width: 200,
-                      decoration: const BoxDecoration(
-                        color: grey,
-                        borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(16.0),
-                          topRight: Radius.circular(16.0),
-                          bottomLeft: Radius.circular(16.0),
-                          bottomRight: Radius.circular(64.0),
+                // Card Container.
+                Container(
+                  height: 220,
+                  width: 200,
+                  decoration: const BoxDecoration(
+                    color: grey,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(16.0),
+                      topRight: Radius.circular(16.0),
+                      bottomLeft: Radius.circular(16.0),
+                      bottomRight: Radius.circular(96.0),
+                    ),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 16.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Text(
+                          'Chocolate chips',
+                          softWrap: true,
+                          style: TextStyle(
+                            fontSize: 32,
+                          ),
                         ),
+                        const SizedBox(height: 10.0),
+                        Row(
+                          children: [
+                            Image.asset(
+                              'assets/crown.png',
+                              height: 24.0,
+                              width: 24.0,
+                              color: darkOrange,
+                            ),
+                            const SizedBox(width: 10.0),
+                            Text(
+                              'Premium'.toUpperCase(),
+                              style: const TextStyle(
+                                color: darkOrange,
+                                fontSize: 18,
+                              ),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(height: 10.0),
+                        const Text(
+                          '\$20 USD',
+                          style: TextStyle(
+                              fontSize: 22.0,
+                              fontWeight: FontWeight.w600
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                // Icon Button.
+                Positioned.fill(
+                  child: Align(
+                    alignment: Alignment.bottomRight,
+                    child: GestureDetector(
+                      onTap: () {},
+                      child: Container(
+                        height: 60.0,
+                        width: 60.0,
+                        decoration: BoxDecoration(
+                            color: black,
+                            borderRadius: BorderRadius.circular(160.0)
+                        ),
+                        child: const Icon(Icons.arrow_forward),
                       ),
                     ),
                   ),
                 ),
-                Image.asset(
-                  'assets/chocolate_chip_cookie.png',
-                  fit: BoxFit.cover,
-                  height: 180,
-                  width: 180,
-                ),
-                Positioned(
+                // Cookie Image.
+                Positioned.fill(
                   child: Align(
-                    alignment: Alignment.center,
-                    child: IconButton(
-                      onPressed: () {},
-                      style: const ButtonStyle(
-                        backgroundColor: MaterialStatePropertyAll(black),
-                      ),
-                      icon: const Icon(
-                        Icons.arrow_forward,
-                        color: white,
-                      ),
+                    alignment: Alignment.topCenter,
+                    child: Image.asset(
+                      'assets/chocolate_chip_cookie.png',
+                      fit: BoxFit.cover,
+                      height: 150,
+                      width: 150,
                     ),
                   ),
                 ),
